e9bbe20fb6628de725842ad9e9caa7c7b42659d7
defghij <norrisc@protonmail.com>
2021-01-07 16:35:39 -0500

    Added check to see if VIM and TMUX are installed-- install if arent.

--- setup.sh
+++ setup.sh
@@ -2,10 +2,30 @@
 
 echo "### Setting up environment ####"
 
+VIM_DFAULT_DIR="/usr/bin/vim"
+TMUX_DFAULT_DIR="/usr/bin/tmux"
+
 # Get current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Current directory: $DIR"
 
+# Check that vim and tmux exist. If so then copy files
+echo "Checking that Vim is installed"
+if [ ! -f "$VIM_DFAULT_DIR" ]
+  then
+    echo "Vim is not installed."
+    sudo apt get vim
+  else 
+    echo "Vim is installed"
+fi
+echo "Checking that TMux is installed"
+if [ ! -f "$VIM_DFAULT_DIR" ]
+  then
+    echo "TMux is not installed."
+    sudo apt get tmux
+  else 
+    echo "TMux is installed"
+fi
 
 # Copy the dot files to the home directory. 
 echo "creating sym links: ~/<dot-files> -> $DIR"
